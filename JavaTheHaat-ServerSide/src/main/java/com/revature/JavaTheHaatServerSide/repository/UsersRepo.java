281db54417307481fde4afee325388b8af0b810e
Alicia <aliciadouglas26@gmail.com>
2018-09-10 13:40:29 -0400

    fixed build issue

--- JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/repository/UsersRepo.java
+++ JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/repository/UsersRepo.java
@@ -6,6 +6,17 @@ import com.revature.JavaTheHaatServerSide.beans.Users;
 
 public interface UsersRepo extends CrudRepository<Users, Integer>{
 	
+	/**
+	 * find user by uId
+	 * @param uId
+	 * @return
+	 */
 	Users findByUId(int uId);
 
+	/**
+	 * find user by username
+	 * @param username
+	 * @return
+	 */
+	Users findByUsername(String username);
 }
