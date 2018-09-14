10b1b34f91dbf63efa01f6b0b7cd7ceac6030f3d
Alicia <aliciadouglas26@gmail.com>
2018-09-14 13:14:29 -0400

    added swagger

--- JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/ctrl/CommentsCtrl.java
+++ JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/ctrl/CommentsCtrl.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.JavaTheHaatServerSide.beans.Comments;
@@ -37,5 +38,5 @@ public class CommentsCtrl {
 		System.out.println("/comments - DELETE");
 		commentsService.deleteComment(comment);
 	}
-	
+
 }
