21b98dc83b7345bce0fe26b070b76c1b202fb8ae
Alicia <aliciadouglas26@gmail.com>
2018-09-15 11:07:28 -0400

    working on logging

--- JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/aspect/LoggingAspect.java
+++ JavaTheHaat-ServerSide/src/main/java/com/revature/JavaTheHaatServerSide/aspect/LoggingAspect.java
@@ -2,33 +2,20 @@ package com.revature.JavaTheHaatServerSide.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+
 @Aspect
+@Configuration
 public class LoggingAspect {
 
-	private static Logger logger = Logger.getLogger(LoggingAspect.class);
+	final static Logger logger = Logger.getLogger(LoggingAspect.class);
 
-	/**
-	 * Add log after throwing for all methods
-	 * @param jp
-	 * @param e
-	 */
-	@AfterThrowing(pointcut = "execution(* *(..))", throwing = "e")
-	public void afterThrowingLog(JoinPoint jp, Throwable e) {
-		System.out.println("hitting after throwing aspect");
-		logger.error("Exception in: " + jp.getSignature().getName()
-				+ "\n\nThe exception is: "
-				+ e.getMessage(), e);
-	}
-	
-	@Before("execution(* *(..))")
-	public void beforeThrowing(JoinPoint jp) {
-		System.out.println("Before");
+	@After("execution(* *(..))")
+	public void logService(JoinPoint jp) throws Throwable {
+		
 	}
 	
 }
