1e5d6bd15ad22d2def9f5a532d080905d5ae7ba0
Alicia <aliciadouglas26@gmail.com>
2018-09-16 18:26:25 -0400

    added junit

--- JavaTheHaat-ServerSide/src/test/java/com/revature/JavaTheHaatServerSide/UsersTest.java
+++ JavaTheHaat-ServerSide/src/test/java/com/revature/JavaTheHaatServerSide/UsersTest.java
@@ -18,9 +18,11 @@ import com.revature.JavaTheHaatServerSide.beans.Users;
 import com.revature.JavaTheHaatServerSide.repository.UsersRepo;
 import com.revature.JavaTheHaatServerSide.service.UsersService;
 
+import io.restassured.RestAssured;
+
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(webEnvironment= WebEnvironment.NONE)
-public class RegisterUserTest {
+public class UsersTest {
 
 	@Mock
 	UsersRepo userRepo;
@@ -33,17 +35,17 @@ public class RegisterUserTest {
 		MockitoAnnotations.initMocks(this);
 	}
 	
+	/**
+	 * Tests the create a new user method
+	 */
 	@Test
 	public void createUserTest() {
 		Users user = new Users();
 		user.setFname("Alicia");
 		user.setLname("Douglas");
-		
 		when(userRepo.save(any(Users.class))).thenReturn(user);
-		
-		Users newUser = userService.registerUser(user);
-		System.out.println(newUser);
-		
+		Users newUser = userService.registerUser(user);		
 		assertEquals("Alicia", newUser.getFname());
 	}
+	
 }
--- JavaTheHaat-ServerSide/src/test/java/com/revature/JavaTheHaatServerSide/test/Tests.java
+++ JavaTheHaat-ServerSide/src/test/java/com/revature/JavaTheHaatServerSide/test/Tests.java
@@ -7,14 +7,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.revature.JavaTheHaatServerSide.JavaTheHaatServerSideApplication;
-import com.revature.JavaTheHaatServerSide.RegisterUserTest;
+import com.revature.JavaTheHaatServerSide.UsersTest;
 
 import io.restassured.RestAssured;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes=RegisterUserTest.class)
+@SpringBootTest(classes=UsersTest.class)
 @ContextConfiguration(classes=JavaTheHaatServerSideApplication.class)
-public class Tests extends RegisterUserTest{
+public class Tests extends UsersTest{
 	
 	static String url = "http://localhost:8080";
 	
